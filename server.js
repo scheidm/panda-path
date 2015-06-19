b67ecd0aef37145b96ec92f0f8f387abfed79b6f
scheidm <scheidm@wellcomposed.net>
2015-06-19 14:00:58 -0400

    sql foibles figured out, now able to create and save to database

--- server.js
+++ server.js
@@ -43,29 +43,95 @@ function download_character_render(server,character){
   })
   console.log("download character render complete");
 }
-function quest_update(server, character, res){
-  var config = require('./config')
+function api_call(server, character, fields, cb){
+  console.log("api_call");
+  var config = require('./config');
   var util = require('util');
   var request = require('request');
-	var url = util.format('http://us.battle.net/api/wow/character/%s/%s?fields=quests&locale=en_US&apikey=%s',server,character,config.api_key);
+	var url = util.format('http://us.battle.net/api/wow/character/%s/%s?fields=%s&locale=en_US&apikey=%s', server, character, fields, config.api_key);
 	request(url, function(error, response, json ){
 		if(!error){
-      var fs = require('fs');
-      var filename=util.format("%s-%s-quest.json",server,character);
-      fs.writeFile(filename,json, function(err) {
-        if(err) {
-          return console.log(err);
-        }
-        console.log("The file was saved!");
-      }); 
+        j=JSON.parse(json);
+        cb(j);
     }
   });
+  console.log("api_call");
+}
+function gear_check(j){
+  console.log("gear_check");
+  var columns=["head","neck","shoulder","back","chest","shirt","tabard","wrist","hands","waist","legs","feet","finger1","finger2","trinket1","trinket2","mainHand","offHand"];
+  for ( var n in columns ){
+    if(!j.items[ columns[n] ]){
+      j.items[ columns[n] ]={id: "NULL"};
+    }
+  }
+  j.name='"'+j.name+'"';
+  j.realm='"'+j.realm+'"';
+  var util = require('util');
+	var statement = util.format( 'INSERT INTO current_gear VALUES ( %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s,%s, %s, %s, %s, %s );',
+    j.name,
+    j.realm,
+    j.items.head.id,
+    j.items.neck.id,
+    j.items.shoulder.id,
+    j.items.back.id,
+    j.items.chest.id,
+    j.items.shirt.id,
+    j.items.tabard.id,
+    j.items.wrist.id,
+    j.items.hands.id,
+    j.items.waist.id,
+    j.items.legs.id,
+    j.items.feet.id,
+    j.items.finger1.id,
+    j.items.finger2.id,
+    j.items.trinket1.id,
+    j.items.trinket2.id,
+    j.items.mainHand.id,
+    j.items.offHand.id);
+  db_save(statement); 
+  console.log("gear_check");
+}
+
+function db_save(statement){
+  var fs = require("fs");
+  var db_file = "pandaren.db";
+  var sqlite3 = require("sqlite3").verbose();
+  var db = new sqlite3.Database(db_file);
+  db.serialize(function(){
+    console.log("db_save");
+    console.log(statement);
+    console.log('state');
+    stmt = db.prepare(statement);
+    stmt.run();
+    stmt.finalize();
+  });
+  db.close();
+  console.log("db_save");
+}
+function dbCreate(){
+  console.log("dbCreate");
+  var fs = require("fs");
+  var db_file = "pandaren.db";
+  var sqlite3 = require("sqlite3").verbose();
+  var db = new sqlite3.Database(db_file);
+  var seed = fs.readFile('createDB.sql', 'utf8', function (err,data) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log(data);
+    db.run(data);
+    db.close();
+  });
+  console.log("dbCreate");
 }
 
 app.get('/scrape', function(req, res){
-  var character = quest_update('ysera', 'Mysunanstars');
-
+  //dbCreate();
+  console.log('main');
+  api_call("ysera", "mysunanstars", "items", gear_check);
   
+  //var character = quest_update('ysera', 'Mysunanstars');
   //download_character_render("firetree","Corradhledo"); 
   res.send('Check your console!')
 })
