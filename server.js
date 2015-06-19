c41d3a1caef3c1b52d1990d4d6a634b23ec901a7
scheidm <scheidm@wellcomposed.net>
2015-06-19 07:54:53 -0400

    now pulling basic quest data from wow api and saving to file

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ function curl(url, file){
   var util = require('util');
   var exec = require('child_process').exec;
   var command = util.format('curl %s > %s', url, file);
+  console.log(command);
   exec(command, function(error, stdout, stderr){
 
     console.log('stdout: ' + stdout);
@@ -42,8 +43,28 @@ function download_character_render(server,character){
   })
   console.log("download character render complete");
 }
+function quest_update(server, character, res){
+  var config = require('./config')
+  var util = require('util');
+  var request = require('request');
+	var url = util.format('http://us.battle.net/api/wow/character/%s/%s?fields=quests&locale=en_US&apikey=%s',server,character,config.api_key);
+	request(url, function(error, response, json ){
+		if(!error){
+      var fs = require('fs');
+      var filename=util.format("%s-%s-quest.json",server,character);
+      fs.writeFile(filename,json, function(err) {
+        if(err) {
+          return console.log(err);
+        }
+        console.log("The file was saved!");
+      }); 
+    }
+  });
+}
 
 app.get('/scrape', function(req, res){
+  var character = quest_update('ysera', 'Mysunanstars');
+
   
   //download_character_render("firetree","Corradhledo"); 
   res.send('Check your console!')
